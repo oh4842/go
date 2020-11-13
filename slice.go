63c9f7e1949a825859fce448cefd1d9613862191
oh4842 <oh4842@naver.com>
2020-11-13 14:23:05 +0900

    v2

--- slice.go
+++ slice.go
@@ -4,11 +4,14 @@ import "fmt"
 
 func main(){
 	array3 := [5]string{"a","b","c","d","e"}
-	var slice3, slice4 = []string
+	var slice3, slice4 []string
+	slice3 = make ([]string, 3) 	
+	slice3 = array3[0:3]
+	slice4 = array3[0:3]
 	//slice3 := array3[0:3]
 	//slice4 := array3[2:5]
 	fmt.Println(slice3, slice4)
-	//slice3[2] := "Queen"
-	//fmt.Println(slice3, slice4)
+	array3[2] = "Queen"
+	fmt.Println(slice3, slice4)
 	fmt.Println(array3)
 }
