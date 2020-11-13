4b7935e17f03c00352c54e6fa9efc5ad6ccc9654
oh4842 <oh4842@naver.com>
2020-11-13 14:15:21 +0900

    v1

--- slice.go
+++ slice.go
@@ -3,16 +3,12 @@ package main
 import "fmt"
 
 func main(){
-	array1 := [5]string{"i", "n", "h", "a", "!"}
 	array3 := [5]string{"a","b","c","d","e"}
-	slice3 := array3[0:3]
-	slice4 := array3[2:5]
+	var slice3, slice4 = []string
+	//slice3 := array3[0:3]
+	//slice4 := array3[2:5]
 	fmt.Println(slice3, slice4)
-	//array3[2] = "z"
-	slice1 := array1[0:3]
-	fmt.Println(slice3, slice4)
-	fmt.Println(slice1)
-
-	slice3[2] := "Queen"
-	fmt.Println(slice3)
+	//slice3[2] := "Queen"
+	//fmt.Println(slice3, slice4)
+	fmt.Println(array3)
 }
